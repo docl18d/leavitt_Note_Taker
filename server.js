31d2e51116d12baedbac86d50f1e42eddd7da429
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2020-11-28 14:33:16 -0700

    cleaning up server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 // Dependencies
 // =============================================================
 const express = require("express");
-const fs = require("fs");
-
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3001
 
+
 // Sets up the Express app to handle data parsing
 // =============================================================
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/styles", express.static("./styles"));
+app.use("/assets", express.static("./assets"))
 
 
 require("./routing/html_routes")(app);
