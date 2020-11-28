dae3a3dcff4ba189f06081434fc3fe852547fabc
Adrein Leavitt <68399114+docl18d@users.noreply.github.com>
2020-11-28 15:12:01 -0700

    made some changes to files in api_routes.js

---
+++ db/db.js
@@ -0,0 +1,15 @@
+var note_data = [
+    {
+    "title":"Test Title 1",
+    "text":"Test text",
+    "id":1
+    },
+    {
+    "title":"Test Title 2",
+    "text":"Test text",
+    "id":2
+    }
+    
+];
+
+module.exports = note_data;
